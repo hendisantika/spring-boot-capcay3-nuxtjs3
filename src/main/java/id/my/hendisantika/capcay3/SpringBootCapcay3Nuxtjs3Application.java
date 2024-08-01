92e6c911353ae136793176587a7c16e1dd1713bf
hendisantika <hendisantika@yahoo.co.id>
2024-08-01 08:44:18 +0700

    Add restTemplate🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/capcay3/SpringBootCapcay3Nuxtjs3Application.java
+++ src/main/java/id/my/hendisantika/capcay3/SpringBootCapcay3Nuxtjs3Application.java
@@ -2,6 +2,9 @@ package id.my.hendisantika.capcay3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootCapcay3Nuxtjs3Application {
@@ -10,4 +13,8 @@ public class SpringBootCapcay3Nuxtjs3Application {
         SpringApplication.run(SpringBootCapcay3Nuxtjs3Application.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
