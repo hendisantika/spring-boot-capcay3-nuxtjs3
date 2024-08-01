e1a8768e68ae703941e0eb88f6e418ee9bdbd3bf
hendisantika <hendisantika@yahoo.co.id>
2024-08-01 08:38:44 +0700

    Add verifyRecaptcha in GoogleRecaptchaService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/capcay3/service/GoogleRecaptchaService.java
+++ src/main/java/id/my/hendisantika/capcay3/service/GoogleRecaptchaService.java
@@ -1,10 +1,21 @@
 package id.my.hendisantika.capcay3.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.capcay3.dto.GoogleRecaptchaRequest;
+import id.my.hendisantika.capcay3.dto.GoogleRecaptchaResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +37,30 @@ public class GoogleRecaptchaService {
     private final ObjectMapper objectMapper;
     private final RestTemplate restTemplate;
 
+    public GoogleRecaptchaResponse verifyRecaptcha(GoogleRecaptchaRequest request) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("secret", request.getSecret());
+            map.add("response", request.getResponse());
+            map.add("remoteip", request.getRemoteip());
+            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
+            ResponseEntity<GoogleRecaptchaResponse> response = restTemplate.exchange(RECAPTCHA_VERIFY_ADDRESS,
+                    HttpMethod.POST,
+                    entity,
+                    GoogleRecaptchaResponse.class);
+            GoogleRecaptchaResponse googleResponse = response.getBody();
+            if (Objects.isNull(googleResponse)) {
+                throw new RuntimeException("Google response is null");
+            }
+            if (Objects.nonNull(googleResponse.getScore()) && googleResponse.getScore() < SCORE_THRESHOLD) {
+                log.warn("User score is lower than threshold. GoogleResponse :: {}", googleResponse);
+                googleResponse.setSuccess(false);
+            }
+            return response.getBody();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
