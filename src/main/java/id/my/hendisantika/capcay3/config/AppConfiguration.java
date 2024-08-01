0994def714b641e50fa93af8e9a824df90fffc51
hendisantika <hendisantika@yahoo.co.id>
2024-08-01 08:42:53 +0700

    Add corsFilter in AppConfiguration🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/capcay3/config/AppConfiguration.java
+++ src/main/java/id/my/hendisantika/capcay3/config/AppConfiguration.java
@@ -1,9 +1,17 @@
 package id.my.hendisantika.capcay3.config;
 
+import id.my.hendisantika.capcay3.filter.GoogleRecaptchaFilter;
 import id.my.hendisantika.capcay3.service.GoogleRecaptchaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.time.Duration;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +29,20 @@ public class AppConfiguration {
     private final GoogleRecaptchaService googleRecaptchaService;
     private final HandlerExceptionResolver handlerExceptionResolver;
 
+    @Bean
+    public GoogleRecaptchaFilter googleRecaptchaFilter() {
+        return new GoogleRecaptchaFilter(googleRecaptchaService, handlerExceptionResolver);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*")); // add this line with appropriate methods for your case
+        corsConfiguration.setMaxAge(Duration.ofMinutes(10));
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 }
