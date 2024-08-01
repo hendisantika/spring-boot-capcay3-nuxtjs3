c05499e397822ea446f8ab5e427b180443564241
hendisantika <hendisantika@yahoo.co.id>
2024-08-01 08:43:30 +0700

    Add securityFilterChain in AppConfiguration🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/capcay3/config/AppConfiguration.java
+++ src/main/java/id/my/hendisantika/capcay3/config/AppConfiguration.java
@@ -5,6 +5,9 @@ import id.my.hendisantika.capcay3.service.GoogleRecaptchaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -44,5 +47,21 @@ public class AppConfiguration {
         corsConfiguration.setMaxAge(Duration.ofMinutes(10));
         source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(source);
+    }
+
+    /**
+     * WARNING: Do not use this configuration as it is!!!
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/**").permitAll();
+
+        return http.build();
     }
 }
